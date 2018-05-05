d4f5561be6a908c26b5f06b205d7f061096d3e5a
Logan Wright <logan.william.wright@gmail.com>
2018-05-05 11:12:16 -0500

    fix feature unwrapping, update sword

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "PennyDiscord",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/loganwright/Sword", from: "0.9.1"),
+        .package(url: "https://github.com/loganwright/Sword", from: "0.9.2"),
     ],
     targets: [
         .target(
